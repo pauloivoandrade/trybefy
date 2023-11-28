0a79676894fea900b4f99b60ea0de3f87a4c9cec
pauloivoandrade <pauloivoasantos@gmail.com>
2023-11-28 12:55:03 -0300

    Quesito 28

---
+++ src/28_challenge.sql
@@ -0,0 +1,11 @@
+SELECT
+    users.full_name AS 'Pessoa usuária',
+    COUNT(artists_followers.artist_id) AS 'Artistas que segue'
+FROM
+    users
+LEFT JOIN
+    artists_followers ON users.id = artists_followers.user_id
+GROUP BY
+    users.id, users.full_name
+ORDER BY
+    users.full_name ASC;
