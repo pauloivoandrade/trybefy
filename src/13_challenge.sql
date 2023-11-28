6aeccf0a7fda9264798f8318f6cc810c3907303f
pauloivoandrade <pauloivoasantos@gmail.com>
2023-11-28 11:13:10 -0300

    Quesito 13

---
+++ src/13_challenge.sql
@@ -0,0 +1,12 @@
+SELECT 
+    artists.name AS 'Artista'
+FROM 
+    artists
+JOIN
+    albums ON artists.id = albums.artist_id
+GROUP BY
+    artists.id, artists.name
+HAVING
+    COUNT(albums.id) >= 3
+ORDER BY
+    artists.name ASC;
