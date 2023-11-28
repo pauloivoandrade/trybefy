4d4f5d28ca35c965fb6907cd72442ee97844d6df
pauloivoandrade <pauloivoasantos@gmail.com>
2023-11-28 10:27:31 -0300

    Quesito 09

--- src/09_challenge.sql
+++ src/09_challenge.sql
@@ -1,1 +1,9 @@
--- SELECT COUNT ()
+SELECT 
+    plans.name AS 'Plano',
+    COUNT(users.plan_id) AS 'Quantidade de usuários'
+FROM 
+    users
+JOIN
+    plans ON users.plan_id = plans.id
+GROUP BY 
+    plans.name;
