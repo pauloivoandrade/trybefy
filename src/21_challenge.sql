80fc2da5a111f7d94ea54b70c5d812c18a564cd3
pauloivoandrade <pauloivoasantos@gmail.com>
2023-11-28 12:19:19 -0300

    Quesito 21

---
+++ src/21_challenge.sql
@@ -0,0 +1,9 @@
+SELECT
+    albums.title AS 'Álbum',
+    SUM(songs.duration_in_seconds) AS 'Duração'
+FROM
+    albums
+JOIN
+    songs ON albums.id = songs.album_id
+GROUP BY albums.id, albums.title
+ORDER BY SUM(songs.duration_in_seconds) DESC;
